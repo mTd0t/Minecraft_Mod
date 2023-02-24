21bf1bcbb0f212041f02f87bd899d08a227113b5
mTd0t <118159966+mTd0t@users.noreply.github.com>
2023-02-24 10:43:50 +0800

    basic item and textures

--- src/main/java/com/example/tutorialmod/TutorialMod.java
+++ src/main/java/com/example/tutorialmod/TutorialMod.java
@@ -1,9 +1,8 @@
-package net.example.isip;
+package com.example.tutorialmod;
 
+import com.example.tutorialmod.item.ModItems;
 import com.mojang.logging.LogUtils;
-import net.minecraft.client.Minecraft;
 import net.minecraft.world.level.block.Blocks;
-import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -15,46 +14,35 @@ import net.minecraftforge.registries.ForgeRegistries;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(TutorialMod.MOD_ID)
+@Mod(TutorialMod.MODID)
 public class TutorialMod
-
 {
-    //very important commend
-    public static final String MOD_ID = "tutorialmod";
+
+    public static final String MODID = "tutorialmod";
     private static final Logger LOGGER = LogUtils.getLogger();
     public TutorialMod()
     {
-
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(modEventBus);
 
         modEventBus.addListener(this::commonSetup);
 
         MinecraftForge.EVENT_BUS.register(this);
-
     }
 
     private void commonSetup(final FMLCommonSetupEvent event)
     {
 
-        // Some common setup code
-        LOGGER.info("HELLO FROM COMMON SETUP");
-        LOGGER.info("DIRT BLOCK >> {}", ForgeRegistries.BLOCKS.getKey(Blocks.DIRT));
-
     }
 
-
     // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
-    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
     public static class ClientModEvents
     {
-
         @SubscribeEvent
-        public static void onClientSetup(FMLClientSetupEvent event) {
-
-            // Some client setup code
-            LOGGER.info("HELLO FROM CLIENT SETUP");
-            LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
-
+        public static void onClientSetup(FMLClientSetupEvent event)
+        {
         }
     }
 }
